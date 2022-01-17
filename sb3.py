6c0f8587ced495fab8c1050553d093ea588f62bc
Matthew Jones <stridera@gmail.com>
2022-01-17 02:17:24 -0800

    use a stacking grayscale obs

--- sb3.py
+++ sb3.py
@@ -1,43 +1,45 @@
 """Run the robotron environment using Stable Baselines 3 """
 
-from stable_baselines3 import PPO
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
-import wandb
-from wandb.integration.sb3 import WandbCallback
 from robotron import RobotronEnv
+from wandb.integration.sb3 import WandbCallback
+import wandb
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+from stable_baselines3 import PPO
+from gym.wrappers import GrayScaleObservation
 
 
-config = {
-    "policy_type": "MlpPolicy",
-    "total_timesteps": 25000,
-    "env_name": "robotron",
-}
+def main():
+    config = {
+        "policy_type": "MlpPolicy",
+        "total_timesteps": 5_500_000,
+        "lr": 0.000001,
+        "env_name": "robotron",
+    }
 
-run = wandb.init(
-    project="robotron",
-    config=config,
-    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    monitor_gym=True,  # auto-upload the videos of agents playing the game
-    save_code=True,  # optional
-)
+    run = wandb.init(
+        project="robotron",
+        config=config,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        monitor_gym=True,  # auto-upload the videos of agents playing the game
+        save_code=True,  # optional
+    )
 
-
-def make_env():
     env = RobotronEnv()
-    env = Monitor(env)  # record stats such as returns
-    return env
+    env = GrayScaleObservation(env, keep_dim=True)
+    env = DummyVecEnv([lambda: env])
+    env = VecFrameStack(env, 4, channels_order='last')
+
+    model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}", learning_rate=config["lr"])
+    model.learn(
+        total_timesteps=config["total_timesteps"],
+        callback=WandbCallback(
+            gradient_save_freq=100,
+            model_save_path=f"models/{run.id}",
+            verbose=2,
+        ),
+    )
+    run.finish()
 
 
-env = DummyVecEnv([make_env])
-env = VecVideoRecorder(env, f"videos/{run.id}", record_video_trigger=lambda x: x % 2000 == 0, video_length=200)
-model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
-model.learn(
-    total_timesteps=config["total_timesteps"],
-    callback=WandbCallback(
-        gradient_save_freq=100,
-        model_save_path=f"models/{run.id}",
-        verbose=2,
-    ),
-)
-run.finish()
+if __name__ == "__main__":
+    main()
